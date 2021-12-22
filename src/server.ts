5dabbbbbb363d973b8d426032716314f69f5c65e
Elias Gabriel <eliasgabrielcf@gmail.com>
2021-12-22 17:30:46 -0300

    Tratando as exceções

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.get('/', (request, response) => {
   return response.json({
